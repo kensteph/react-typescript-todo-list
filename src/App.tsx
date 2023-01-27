19ebcfecf1005620c12147a2dc4cb20cc915a31f
Kender ROMAIN <kenderromain@gmail.com>
2023-01-27 16:13:53 -0400

    Implement add todo method

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField/InputField';
+import { Todo } from './models/Todo';
 
-const App: React.FC = ()=> {
+const App: React.FC = () => {
   // State for my input
-  const [todo,setTodo] =  useState<string>("");
+  const [todo, setTodo] = useState<string>('');
+  // State to manage my todos
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  // Function to hanldle add todo
+  const handleAdd = (ev: React.FormEvent): void => {
+    // Prevent the form to submit
+    ev.preventDefault();
+    // Verify if the fiel is not empty
+    if (todo) {
+      setTodos([
+        ...todos, //Copy all the existing todo
+        { id: Date.now(), description: todo, completed: false }, // Add the new one
+      ]);
+      // Reset the input
+      setTodo('');
+    }
+  };
+  console.log(todos);
   return (
     <div className="App">
-     <span className='heading'>TO DO LIST</span>
-     <InputField todo={todo} setTodo = {setTodo} />
+      <span className="heading">TO DO LIST</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import './style.css'
+import React from 'react';
+import './style.css';
 
 type Props = {
-  todo:string,
-  setTodo: React.Dispatch<React.SetStateAction<string>>
-}
-const InputField = ({todo,setTodo}:Props) => {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (ev: React.FormEvent) => void;
+};
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className='form'>
-      <input className='input__box' type="text" placeholder='Add to your lists' onChange={(e)=>setTodo(e.target.value)} />
-      <button className='submit__btn' type="submit">Add</button>
+    <form className="form" onSubmit={handleAdd}>
+      <input
+        className="input__box"
+        type="text"
+        placeholder="Add to your lists"
+        onChange={(e) => setTodo(e.target.value)}
+        value={todo}
+      />
+      <button className="submit__btn" type="submit">
+        Add
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputField
+export default InputField;
